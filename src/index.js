41677fdf2d981cefd113412d89bd4226e9e10cab
David Stephens <stephens.a.david@gmail.com>
2019-11-15 09:38:01 -0600

    completed exercise with require

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 "use strict";
-import $ from 'jquery';
-// import {item as eggs, thing} from './practice.js';
-const cart = require('./practice.js');
+const $ = require('jquery');
+const {sayHelloAgain, sayGoodbye} = require('./say-hello.js');
 
 (() => {
    const sayHello = () => "Hello!";
@@ -9,8 +8,7 @@ const cart = require('./practice.js');
    $('body').css({
       'background-color' : 'burlywood',
       'color' : 'firebrick'
-   }).html(`<h1>${sayHello()}</h1>`);
+   }).html(`<h1>${sayHelloAgain()}</h1>`);
 
-   // console.log(eggs.color);
-   console.log(cart.thing.weight);
+
 })();
---
+++ src/say-hello.js
@@ -0,0 +1,5 @@
+const sayHelloAgain = () => "You are treasured and cared for and worth every bit of love that this world can afford you. Have a fantastic day!";
+
+const sayGoodbye = () => "Bye bye!";
+
+module.exports = {sayHelloAgain, sayGoodbye};
